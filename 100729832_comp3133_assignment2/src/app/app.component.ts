44b1e96067dac8d9ea3c313dc33f7e8f1ef0f8c8
Steven <s.coverdale@hotmail.com>
2026-04-12 23:01:13 -0400

    Working through Frontend Nginx issues

--- 100729832_comp3133_assignment2/src/app/app.component.ts
+++ 100729832_comp3133_assignment2/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+// Apollo providers are registered via `apolloProvider` in bootstrap/main.ts
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, HttpClientModule],
   templateUrl: './app.html',
   styleUrls: ['./app.scss']
 })
