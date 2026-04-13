dc978bcad265f2034a4f2993673d4aacf491cbb5
Steven <s.coverdale@hotmail.com>
2026-04-12 20:53:36 -0400

    Frontend adding standalone authGuard and secure employee routes

--- 100729832_comp3133_assignment2/src/app/app.routes.ts
+++ 100729832_comp3133_assignment2/src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+import { EmployeeAddComponent } from './components/employee-add/employee-add.component';
+import { EmployeeViewComponent } from './components/employee-view/employee-view.component';
+import { EmployeeEditComponent } from './components/employee-edit/employee-edit.component';
+
+import { AuthGuard } from './guards/auth.guard';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+
+  {
+    path: 'employees',
+    canActivate: [AuthGuard],
+    children: [
+      { path: '', component: EmployeeListComponent },
+      { path: 'add', component: EmployeeAddComponent },
+      { path: ':id', component: EmployeeViewComponent },
+      { path: ':id/edit', component: EmployeeEditComponent }
+    ]
+  },
+
+  { path: '**', redirectTo: 'login' }
+];
